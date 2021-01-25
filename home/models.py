1d0e47cdfd3048d246a44f50ff7afd27246ed868
Ethan Cundick <50301038+EthanCundick@users.noreply.github.com>
2021-01-25 21:45:13 +0000

    Updated HomePage Model

--- home/models.py
+++ home/models.py
@@ -7,9 +7,13 @@ from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
     header = models.CharField(max_length=33, blank=False, default="Home")
-    body = RichTextField(blank=True)
+    body = models.TextField(max_length=300, blank=True)
+    about_text_one = models.TextField(max_length=1000, blank=True)
+    about_text_two = models.TextField(max_length=1000, blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel("header"),
         FieldPanel('body', classname="full"),
+        FieldPanel('about_text_one'),
+        FieldPanel('about_text_two')
     ]
